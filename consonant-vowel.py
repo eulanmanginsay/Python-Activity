e181293ec5aa1e09b803bd48a0f7293c9c37bf65
eulanmanginsay <57347025+eulanmanginsay@users.noreply.github.com>
2019-11-11 07:48:02 +0800

    Add files via upload

---
+++ consonant-vowel.py
@@ -0,0 +1,7 @@
+ch= input('Letter: ')
+if ch == 'a' and 'e' and 'i' and 'o' and 'u':
+        print("This letter is a vowel.")
+else:
+        print("This letter is a consonant.")
+    
+    
---
+++ squaresum.py
@@ -0,0 +1,6 @@
+def squaresum(n):
+    sqsm=0
+    for i in range(1, n+1): 
+        sqsm= sqsm + (i*i)
+
+    return sqsm
